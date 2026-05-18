57e89912d472d2ce86132f7ae98246a0c63be357
sk <content.type.text.net@gmail.com>
2026-05-18 20:48:22 +0900

    1.0.6

--- Elin.Plugin.Main/Plugin.cs
+++ Elin.Plugin.Main/Plugin.cs
@@ -18,20 +18,13 @@ namespace Elin.Plugin.Main
         /// <summary>
         /// 起動時のプラグイン独自処理。
         /// </summary>
-        private void AwakePlugin()
+        protected override void AwakePlugin()
         {
             var setting = LoadConfig();
             Setting.Instance = setting;
             // 設定のビルド自体はプロファイル読み込み時に行う
         }
 
-        /// <summary>
-        /// 終了時のプラグイン独自処理。
-        /// </summary>
-        private void OnDestroyPlugin()
-        {
-            //NOP
-        }
 
 #if DEBUG
         public void PHL()
